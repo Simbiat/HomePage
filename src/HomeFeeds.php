ddafd0191ef6cdf389cbcaff369ff5a577f089b2
Simbiat <simbiat@outlook.com>
2021-04-07 20:41:31 +0300

    Atom feeds

--- src/HomeFeeds.php
+++ src/HomeFeeds.php
@@ -12,11 +12,91 @@ class HomeFeeds
         } else {
             return match(strtolower($uri[0])) {
                 'sitemap' => $this->sitemap(array_slice($uri, 1)),
-                'atom' => '',
-                'rss' => '',
+                'atom' => $this->feed(array_slice($uri, 1), 'atom'),
+                'rss' => $this->feed(array_slice($uri, 1), 'rss'),
                 default => ['http_error' => 404],
             };
         }
+    }
+    
+    #Generate Atom/RSS
+    private function feed(array $uri, string $format = 'atom'): array
+    {
+        #Check if empty
+        if (!empty($uri[0])) {
+            $uri[0] = strtolower($uri[0]);
+            $title = match($uri[0]) {
+                'bicchanged' => 'Изменения банков',
+                'bicdeleted' => 'Удаленные банки',
+                default => '',
+            };
+            #Check that type is supported based on existence of the title
+            if (!empty($title)) {
+                #Set general settings first for feeds. Using one array for both types of feeds
+                if ($format === 'atom') {
+                    $settings = [
+                        'authors' => [[
+                            'name' => $GLOBALS['siteconfig']['adminname'],
+                            'email' => $GLOBALS['siteconfig']['adminmail'],
+                            'uri' => 'https://'.$_SERVER['HTTP_HOST'].'/',
+                        ]],
+                        'icon' => 'https://'.$_SERVER['HTTP_HOST'].'/frontend/images/favicons/simbiat.png',
+                        'logo' => 'https://'.$_SERVER['HTTP_HOST'].'/frontend/images/ogimage.png',
+                    ];
+                } elseif ($format === 'rss') {
+                    $settings = [
+                        'webMaster' => $GLOBALS['siteconfig']['adminmail'],
+                        'managingEditor' => $GLOBALS['siteconfig']['adminmail'],
+                        'language' => 'en-us',
+                        'ttl' => 3600,
+                        'image' => [
+                            'url' => 'https://'.$_SERVER['HTTP_HOST'].'/frontend/images/favicons/android-chrome-144x144.png',
+                            'width' => 144,
+                            'height' => 144,
+                        ],
+                    ];
+                }
+                #Set description
+                $description = match($uri[0]) {
+                    'bicchanged' => 'Последние 25 изменений банков',
+                    'bicdeleted' => 'Последние 25 удаленных банков',
+                    default => '',
+                };
+                #Add it to settings
+                if (!empty($description)) {
+                    if ($format === 'atom') {
+                        $settings['subtitle'] = $description;
+                    } elseif ($format === 'rss') {
+                        $settings['description'] = $description;
+                    }
+                }
+                #Change language for BICs
+                if ($format === 'rss' && in_array($uri[0], ['bicchanged', 'bicdeleted'])) {
+                    $settings['language'] = 'ru-ru';
+                }
+                #Set query for the feed
+                if ($format === 'atom') {
+                    $query = match($uri[0]) {
+                        'bicchanged' => 'SELECT CONCAT(\'https://'.$_SERVER['HTTP_HOST'].'/bictracker/bic/\', `VKEY`) as `link`, `NAMEP` as `title`, `DT_IZM` as `updated`, \'Central Bank of Russia\' AS `author_name`, \'https://cbr.ru/\' AS `author_uri`, `NAMEP` as `summary`, `DT_IZM` as `published`, \'Central Bank of Russia\' AS `source_title`, \'https://cbr.ru/\' AS `source_id`, `DT_IZM` as `source_updated` FROM `bic__list` a WHERE `DATEDEL` IS NULL ORDER BY `DT_IZM` DESC LIMIT 25',
+                        'bicdeleted' => 'SELECT CONCAT(\'https://'.$_SERVER['HTTP_HOST'].'/bictracker/bic/\', `VKEY`) as `link`, `NAMEP` as `title`, `DT_IZM` as `updated`, \'Central Bank of Russia\' AS `author_name`, \'https://cbr.ru/\' AS `author_uri`, `NAMEP` as `summary`, `DT_IZM` as `published`, \'Central Bank of Russia\' AS `source_title`, \'https://cbr.ru/\' AS `source_id`, `DT_IZM` as `source_updated` FROM `bic__list` a WHERE `DATEDEL` IS NOT NULL ORDER BY `DATEDEL` DESC LIMIT 25',
+                    };
+                } elseif ($format === 'rss') {
+                    $query = match($uri[0]) {
+                        'bicchanged' => 'SELECT CONCAT(\'https://'.$_SERVER['HTTP_HOST'].'/bictracker/bic/\', `VKEY`) as `link`, `NAMEP` as `title`, `DT_IZM` as `pubDate`, \'BICs\' AS `category` FROM `bic__list` a WHERE `DATEDEL` IS NULL ORDER BY `DT_IZM` DESC LIMIT 25',
+                        'bicdeleted' => 'SELECT CONCAT(\'https://'.$_SERVER['HTTP_HOST'].'/bictracker/bic/\', `VKEY`) as `link`, `NAMEP` as `title`, `DT_IZM` as `pubDate`, \'BICs\' AS `category` FROM `bic__list` a WHERE `DATEDEL` IS NOT NULL ORDER BY `DATEDEL` DESC LIMIT 25',
+                    };
+                    
+                }
+                #Generate the feed
+                if ($format === 'atom') {
+                    (new \Simbiat\http20\Atom)->Atom($GLOBALS['siteconfig']['site_name'].': '.$title, (new \Simbiat\Database\Controller)->selectAll($query), feed_settings: $settings);
+                } elseif ($format === 'rss') {
+                    (new \Simbiat\http20\RSS)->RSS($GLOBALS['siteconfig']['site_name'].': '.$title, (new \Simbiat\Database\Controller)->selectAll($query), feed_settings: $settings);
+                }              
+            }
+        }
+        #If we reach here, it means, the requested page does not exist
+        return ['http_error' => 404];
     }
     
     #Generate sitemap
